0253dafdb76b76cea66e506760e87ef4a6e87f10
rickyganiere <riccardoganiere@gmail.com>
2026-09-21 22:16:10 +0200

    Fix Worker module newline syntax

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import { SOURCE_REGISTRY } from "./source-registry.js";\n\nfunction json(data, init = {}) {
+import { SOURCE_REGISTRY } from "./source-registry.js";
+
+function json(data, init = {}) {
   const headers = new Headers(init.headers || {});
   headers.set("content-type", "application/json; charset=utf-8");
   headers.set("cache-control", "no-store");
